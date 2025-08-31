51602aef322832c03f0499e214790cd39b5fb701
ImaniDigitalDiary <ImaniDigitalDiary@gmail.com>
2025-08-31 00:59:05 -0400

    added error handling middleware

--- backend/src/middleware/errorHandler.js
+++ backend/src/middleware/errorHandler.js
@@ -1,1 +1,12 @@
 // Error handling middleware
+
+
+const errorHandler = (err, req, res, next) => {
+    console.error(err.stack)
+    res.status(err.statusCode || 500).json({
+        success: false,
+        message: err.message || 'Server Error',
+    })
+}
+
+export default errorHandler
