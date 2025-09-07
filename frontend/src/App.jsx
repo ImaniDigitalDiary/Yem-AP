15e81ed517bb1ba4c57ad7c3beab12f2b0cce208
ImaniDigitalDiary <ImaniDigitalDiary@gmail.com>
2025-09-06 23:12:32 -0400

    added react hot toast and a sample btn to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,11 @@ import HomePage from './pages/Homepage'
 import VendorCreatePage from './pages/VendorCreatePage'
 import VendorDetailPage from './pages/VendorDetailPage'
 
+import toast from 'react-hot-toast'
+
 const App = () => {
   return <div>
+      <button onClick={() => toast.success('congrats')}>Click me</button>
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/create-vendor' element={<VendorCreatePage/>} />
