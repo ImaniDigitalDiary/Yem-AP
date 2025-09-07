d974922a4ea9af41beb5560df0f782f8244b1cf7
ImaniDigitalDiary <ImaniDigitalDiary@gmail.com>
2025-09-06 23:32:02 -0400

    added daisyui into tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,14 +9,22 @@ import VendorDetailPage from './pages/VendorDetailPage'
 import toast from 'react-hot-toast'
 
 const App = () => {
-  return <div>
-      <button onClick={() => toast.success('congrats')}>Click me</button>
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/create-vendor' element={<VendorCreatePage/>} />
-        <Route path='/vendor/:vendorId' element={<VendorDetailPage/>} />
-      </Routes>
-  </div>
+  return (
+    <div data-theme='nord'>
+        <button className="btn">Button</button>
+        <button className="btn btn-neutral">Neutral</button>
+        <button className="btn btn-primary">Primary</button>
+        <button className="btn btn-secondary">Secondary</button>
+        <button className="btn btn-accent">Accent</button>
+        <button className="btn btn-ghost">Ghost</button>
+        <button className="btn btn-link">Link</button>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/create-vendor' element={<VendorCreatePage/>} />
+          <Route path='/vendor/:vendorId' element={<VendorDetailPage/>} />
+        </Routes>
+    </div>
+  )
 }
 
 
