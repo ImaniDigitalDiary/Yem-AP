98b72ca03915986c8319018d193a97fd41285146
ImaniDigitalDiary <ImaniDigitalDiary@gmail.com>
2025-09-17 04:04:14 -0400

    change thead text size to md and centered

--- frontend/src/components/vendor-components/VendorsTable.jsx
+++ frontend/src/components/vendor-components/VendorsTable.jsx
@@ -4,7 +4,7 @@ const VendorsTable = ({vendors}) => {
   return (
     <div className='relative overflow-x-auto shadow-md sm:rounded-lg'>
         <table className='w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400'>
-            <thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
+            <thead className='text-md text-center text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
                 <tr>
                     <th>Vendor Name</th>
                     <th>Email</th>
@@ -16,6 +16,7 @@ const VendorsTable = ({vendors}) => {
                     <tr key={vendor._id}>
                         <th>{vendor.vendorName}</th>
                         <td>{vendor.vendorEmail}</td>
+                        <td></td>
                     </tr>
                 ))}
                 
