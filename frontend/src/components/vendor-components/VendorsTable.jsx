71d179f765f192d3b69c94930801aea4d68b35e2
ImaniDigitalDiary <ImaniDigitalDiary@gmail.com>
2025-09-16 23:27:24 -0400

    imported VendorsTable comp into VendorHomePage

--- frontend/src/components/vendor-components/VendorsTable.jsx
+++ frontend/src/components/vendor-components/VendorsTable.jsx
@@ -4,7 +4,7 @@ const VendorsTable = () => {
   return (
     <div className='relative overflow-x-auto shadow-md sm:rounded-lg'>
         <table className='w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400'>
-            <thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark: text-gray-400'>
+            <thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
                 <tr>
                     <th>Vendor Name</th>
                     <th>Email</th>
@@ -13,7 +13,7 @@ const VendorsTable = () => {
             </thead>
             <tbody>
                 <tr>
-                    
+
                 </tr>
             </tbody>
 
