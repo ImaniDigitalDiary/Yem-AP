11a62d99fb59c19d7ab460f700b74b8bdeb920fa
ImaniDigitalDiary <ImaniDigitalDiary@gmail.com>
2025-09-16 23:12:52 -0400

    created VendorsTable component and put test headers into columns

---
+++ frontend/src/components/vendor-components/VendorsTable.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const VendorsTable = () => {
+  return (
+    <div className='relative overflow-x-auto shadow-md sm:rounded-lg'>
+        <table className='w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400'>
+            <thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark: text-gray-400'>
+                <tr>
+                    <th>Vendor Name</th>
+                    <th>Email</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    
+                </tr>
+            </tbody>
+
+        </table>
+      
+    </div>
+  )
+}
+
+export default VendorsTable
