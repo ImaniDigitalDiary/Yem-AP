bce4c085fa0b527724a9465cd2f9cbc1a6cd474e
ImaniDigitalDiary <ImaniDigitalDiary@gmail.com>
2025-09-21 03:48:32 -0400

    created VendorSearch comp and imported into VendorHomepage

---
+++ frontend/src/components/vendor-components/VendorSearch.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const VendorSearch = () => {
+  return (
+    <div>
+      {/* Search Input */}
+      <input 
+        type="text"
+        placeholder='Search Vendors...'
+        className='p-2'
+        // value={}
+        // onChange={}
+    />
+    </div>
+  )
+}
+
+export default VendorSearch
