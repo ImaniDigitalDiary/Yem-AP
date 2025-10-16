5005dab57b7c081251a00846216df8bdfd767460
ImaniDigitalDiary <ImaniDigitalDiary@gmail.com>
2025-10-16 13:31:57 -0400

    updated the ui for VendorInfoCard

--- frontend/src/components/vendor-components/VendorInfoCard.jsx
+++ frontend/src/components/vendor-components/VendorInfoCard.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import InvoicesNotFound from '../invoice-components/InvoicesNotFound'
 
-import { Link } from 'react-router'
+import { Link, useParams } from 'react-router'
 const VendorInfoCard = ({vendor}) => {
   if (!vendor) return <InvoicesNotFound />
 
+  const {vendorId} = useParams()
   // an array of objects representing each vendor info group. Each group contains an array of objects with label and value properties.
   // all the vendor info that are displayed in the card are fetched from this array.
   const vendorInfoGroups = [
@@ -23,32 +24,25 @@ const VendorInfoCard = ({vendor}) => {
     ],
   ]
   return (
-    <div className='w-full max-w-3xl mx-auto border border-gray-400 rounded-3xl shadow-lg bg-slate-400 p-10'>
-      <h2 className='text-3xl font-bold mb-8 uppercase text-center text-slate-900'>
-        {vendor.vendorName}
-      </h2>
-
-      {/* GRID FOR A 2 COL LAYOUT */}
-      {/* <div className='grid grid-cols-1 sm:grid-cols-2 gap-6'>
-        {vendorInfoGroups.map((vendorInfoGroupItem, index) => (
-          <div key={index} className='flex flex-col'>
-            <span className='font-semibold text-slate-900 mb-1'>
-              {vendorInfoGroupItem.label}
-            </span>
-            <div className='bg-slate-700 text-gray-200 text-lg px-4 py-3 rounded-md break-words'>
-              {vendorInfoGroupItem.value || '-'}
-            </div>
-          </div>
-        ))} */}
+    <div className='w-full max-w-3xl mx-10  border-gray-400 rounded-3xl shadow-xl bg-slate-400 p-10 px-20'>
+      <div key={vendorId} className='text-3xl font-bold mb-8 uppercase text-gray-800 tracking-wide'>
+        {vendor.vendorName }
+        <br />
+        <span className='text-xl pb-3 font-thin'>
+        {/* displays the id of the vendor -would later like to update to not show mongodb id but to render in new array starting at 1 for first vendor and going upwards while still keeping mongodb id on backend */}
+         Vendor ID# <strong>{vendor?._id} </strong>
+      </span>
+      </div>
+      
 
         {vendorInfoGroups.map((group, i) => (
           <div key={i} className='grid grid-cols-1 sm:grid-cols-2 gap-4'>
             {group.map((info, j) => (
               <div key={j} className='flex flex-col'>
-                <span className='font-semibold text-slate-900 mb-1'>
+                <span className='font-thin text-slate-800 mb-0 tracking-wide'>
                   {info.label}
                 </span>
-                <div>
+                <div className='mb-5'>
                   {info.value || '-'}
                 </div>
               </div>
@@ -60,17 +54,36 @@ const VendorInfoCard = ({vendor}) => {
          
 
         {/* LINK TO VENDOR ID INVOICES */}
-        <div>
-          <Link
-            to={`/vendors/${vendor._id}/invoices`}
-            state={{vendor}}
-            className='inline-block bg-slate-700 hover:bg-slate-800 text-white px-6 py-3 rounded-xl transition-all duration-200'
-          >
-            View Invoices
-          </Link>
+        <div className=''>
+          <div className='mt-3'>
+            <Link
+              to={`/vendors/${vendor._id}/invoices`}
+              state={{vendor}}
+              className='inline-block bg-slate-700 hover:bg-slate-800 text-white px-20 py-3 mt-3 rounded-2xl transition-all duration-200'
+            >
+              View Invoices
+            </Link>
+          </div>
         </div>
-      </div>
+
+        </div>
+        
   )
 }
 
 export default VendorInfoCard
+
+
+
+    {/* GRID FOR A 2 COL LAYOUT */}
+      {/* <div className='grid grid-cols-1 sm:grid-cols-2 gap-6'>
+        {vendorInfoGroups.map((vendorInfoGroupItem, index) => (
+          <div key={index} className='flex flex-col'>
+            <span className='font-semibold text-slate-900 mb-1'>
+              {vendorInfoGroupItem.label}
+            </span>
+            <div className='bg-slate-700 text-gray-200 text-lg px-4 py-3 rounded-md break-words'>
+              {vendorInfoGroupItem.value || '-'}
+            </div>
+          </div>
+        ))} */}
