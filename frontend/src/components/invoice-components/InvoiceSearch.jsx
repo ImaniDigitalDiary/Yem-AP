b652c3d878a12ea77794afaa2a5895ab3395847b
ImaniTechDiary <ImaniTechDiary@gmail.com>
2025-10-22 22:55:49 -0400

    added state, handler and input into InvoiceSearch comp

--- frontend/src/components/invoice-components/InvoiceSearch.jsx
+++ frontend/src/components/invoice-components/InvoiceSearch.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const InvoiceSearch = () => {
+// TOAST
+import toast from 'react-hot-toast'
+
+const InvoiceSearch = ({setInvoices}) => {
+    // Implement search functionality here.
+    // When the user types in the search input, update the state of the component
+    const [invoiceSearchQuery, setInvoiceSearchQuery] = useState('')
+    // You can use an event listener or a controlled input component to achieve this.
+    // When the user clicks the search button, make an API call to fetch invoices based on the search query using event handler
+    const handleSearchClick = async (query) =>  { 
+        try {
+            // Make API call to fetch invoices based on the invoiceSearchQuery
+            const reponse = await axiosAPI.get(`/vendors/${vendorId}/invoices?search=${query}`)
+            // Update the state of invoices with the fetched invoices
+            setInvoices(reponse.data)
+        } catch (error) {
+            console.log('Error fetching invoices', error)
+            // if error, clear results to an empty array so that it goes back to the vendor's invoices list
+            if (error.response && error.response.status === 404) {
+                setInvoices([])
+        } else {
+            toast.error('Failed to fetch invoices')
+        }
+    }
+}
+
   return (
     <div>
-      Search invoices
+      <input type='text' placeholder='Search Invoices' />
+      <button onClick={handleSearchClick}>Click to search invoices</button>
     </div>
+
   )
 }
 
